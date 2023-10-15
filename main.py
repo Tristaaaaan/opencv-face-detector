3a84cff1057ea02d619dd982855bf422ba83155f
Mark Tristan R. Fabellar <marktristan.fabellar@cvsu.edu.ph>
2023-10-15 17:24:36 +0800

    backtoHaar

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
-from kivy.lang import Builder
 from kivy.uix.screenmanager import ScreenManager, Screen
-from kivy.graphics.texture import Texture
 
 from kivymd.app import MDApp
-import cv2
-import numpy as np
+
+from kivy.lang import Builder
+from kivy.graphics.texture import Texture
 from kivy.uix.camera import Camera
 from kivy.utils import platform
+
+import numpy as np
+import cv2
 
 if platform == 'android':
     from jnius import autoclass
@@ -27,10 +29,9 @@ if platform == 'android':
 
 
 class AndroidCamera(Camera):
-    net = cv2.dnn.readNet('best.onnx')
-    # Define colors for each class
-    class_colors = [(0, 255, 0)]
-    classes = ["coffee berry"]
+
+    # Trained Model
+    face_cascade = cv2.CascadeClassifier('cascade.xml')
 
     # Resolution
     resolution = (640, 480)
@@ -67,6 +68,8 @@ class AndroidCamera(Camera):
         # Transfer to Screen
         face_detected = self.face_det(frame_rgb)
 
+        self.update_face_status(face_detected)
+
         flipped = np.flip(frame_rgb, 0)
         buf = flipped.tobytes()
         self.texture.blit_buffer(buf, colorfmt='rgb', bufferfmt='ubyte')
@@ -77,56 +80,24 @@ class AndroidCamera(Camera):
         gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
         resized = cv2.resize(
             gray, (self.face_resolution[1], self.face_resolution[0]))
+        faces = self.face_cascade.detectMultiScale(resized, 1.3, 2)
+        if len(faces) != 0:
+            face = faces[np.argmax(faces[:, 3])]
+            x, y, w, h = face
+            cv2.rectangle(frame, (int(x * self.ratio), int(y * self.ratio)),
+                          (int((x + w) * self.ratio), int((y + h) * self.ratio)), (0, 255, 0), 2)
 
-        blob = cv2.dnn.blobFromImage(
-            frame, scalefactor=1/255, size=resized, mean=[0, 0, 0], swapRB=True, crop=False)
+        return len(faces) != 0
 
-        self.net.setInput(blob)
-        detections = self.net.forward()[0]
-
-        classes_ids = [0]
-        confidences = []
-        boxes = []
-        rows = detections.shape[0]
-
-        img_width, img_height = frame.shape[1], frame.shape[0]
-        x_scale = img_width / 640
-        y_scale = img_height / 640
-
-        for i in range(rows):
-            row = detections[i]
-            confidence = row[4]
-            if confidence > 0.5:
-                classes_score = row[5:]
-                ind = np.argmax(classes_score)
-                if classes_score[ind] > 0.5:
-                    classes_ids.append(ind)
-                    confidences.append(confidence)
-                    cx, cy, w, h = row[:4]
-                    x1 = int((cx - w / 2) * x_scale)
-                    y1 = int((cy - h / 2) * y_scale)
-                    width = int(w * x_scale)
-                    height = int(h * y_scale)
-                    box = np.array([x1, y1, width, height])
-                    boxes.append(box)
-
-        indices = cv2.dnn.NMSBoxes(boxes, confidences, 0.5, 0.5)
-
-        for i in indices:
-            x1, y1, w, h = boxes[i]
-            label = self.classes[classes_ids[i]]
-            conf = confidences[i]
-            # Added a space before {:.2f}
-            text = label + " {:.2f}".format(conf)
-            # Get the color for the class
-            class_color = self.class_colors[classes_ids[i]]
-
-            cv2.rectangle(frame, (x1, y1), (x1 + w, y1 + h), class_color, 2)
-            cv2.putText(frame, text, (x1, y1 - 2),
-                        cv2.FONT_HERSHEY_COMPLEX, 0.7, (255, 0, 255), 2)
+    def update_face_status(self, detected):
+        if detected:
+            MDApp.get_running_app().root.first.ids.category.text = "Face detected"
+        else:
+            MDApp.get_running_app().root.first.ids.category.text = "No face detected"
 
 
 class FirstWindow(Screen):
+
     Builder.load_file('firstwindow.kv')
 
 
@@ -135,7 +106,9 @@ class WindowManager(ScreenManager):
 
 
 class rawApp(MDApp):
+
     def build(self):
+
         return WindowManager()
 
 
