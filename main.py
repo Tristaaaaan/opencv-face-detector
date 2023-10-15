0fb7491bc23e21c62c3796c5ad3e5389da94af26
Mark Tristan R. Fabellar <marktristan.fabellar@cvsu.edu.ph>
2023-10-15 15:47:10 +0800

    yolo

--- main.py
+++ main.py
@@ -1,14 +1,14 @@
+from kivy.lang import Builder
 from kivy.uix.screenmanager import ScreenManager, Screen
+from kivy.graphics.texture import Texture
 
 from kivymd.app import MDApp
-
-from kivy.lang import Builder
-from kivy.graphics.texture import Texture
+import threading
+from kivy.clock import Clock
+import cv2
+import numpy as np
 from kivy.uix.camera import Camera
 from kivy.utils import platform
-
-import numpy as np
-import cv2
 
 if platform == 'android':
     from jnius import autoclass
@@ -27,11 +27,12 @@ if platform == 'android':
                     'back': CameraInfo.CAMERA_FACING_BACK}
     index = CAMERA_INDEX['front']
 
+
 class AndroidCamera(Camera):
-
-    # Trained Model
-    face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
-
+    net = cv2.dnn.readNet('best.onnx')
+    # Define colors for each class
+    class_colors = [(0, 255, 0), (0, 0, 255)]
+    classes = ["with mask", "without mask"]
     # Resolution
     resolution = (640, 480)
     face_resolution = (128, 96)
@@ -52,7 +53,7 @@ class AndroidCamera(Camera):
         w, h = self.resolution
         frame = np.frombuffer(self._camera._buffer.tostring(),
                               'uint8').reshape((h + h // 2, w))
-        frame_bgr = cv2.cvtColor(frame, 93)
+        frame_bgr = cv2.cvtColor(frame, cv2.COLOR_YUV2BGR_NV21)
         if self.index:
             return np.flip(np.rot90(frame_bgr, 1), 1)
         else:
@@ -65,47 +66,75 @@ class AndroidCamera(Camera):
         self.counter += 1
 
         # Transfer to Screen
-        face_detected = self.face_det(frame_rgb)
-
-        self.update_face_status(face_detected)
+        self.face_det(frame_rgb)
 
         flipped = np.flip(frame_rgb, 0)
         buf = flipped.tobytes()
         self.texture.blit_buffer(buf, colorfmt='rgb', bufferfmt='ubyte')
 
-    # Detection
+    def face_det(self, frame_rgb):
+        img = cv2.resize(frame_rgb, (1000, 600))
+        blob = cv2.dnn.blobFromImage(
+            img, scalefactor=1/255, size=(640, 640), mean=[0, 0, 0], swapRB=True, crop=False)
 
-    def face_det(self, frame):
-        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-        resized = cv2.resize(
-            gray, (self.face_resolution[1], self.face_resolution[0]))
-        faces = self.face_cascade.detectMultiScale(resized, 1.3, 2)
-        if len(faces) != 0:
-            face = faces[np.argmax(faces[:, 3])]
-            x, y, w, h = face
-            cv2.rectangle(frame, (int(x * self.ratio), int(y * self.ratio)),
-                          (int((x + w) * self.ratio), int((y + h) * self.ratio)), (0, 255, 0), 2)
+        self.net.setInput(blob)
+        detections = self.net.forward()[0]
+        # cx, cy, w, h, confidence, 80 class_scores
+        # class_ids, confidences, boxes
 
-        return len(faces) != 0
+        classes_ids = [0, 1]
+        confidences = []
+        boxes = []
+        rows = detections.shape[0]
 
-    def update_face_status(self, detected):
-        if detected:
-            MDApp.get_running_app().root.first.ids.category.text = "Face detected"
-        else:
-            MDApp.get_running_app().root.first.ids.category.text = "No face detected"
-                    
+        img_width, img_height = img.shape[1], img.shape[0]
+        x_scale = img_width / 640
+        y_scale = img_height / 640
+
+        for i in range(rows):
+            row = detections[i]
+            confidence = row[4]
+            if confidence > 0.5:
+                classes_score = row[5:]
+                ind = np.argmax(classes_score)
+                if classes_score[ind] > 0.5:
+                    classes_ids.append(ind)
+                    confidences.append(confidence)
+                    cx, cy, w, h = row[:4]
+                    x1 = int((cx - w / 2) * x_scale)
+                    y1 = int((cy - h / 2) * y_scale)
+                    width = int(w * x_scale)
+                    height = int(h * y_scale)
+                    box = np.array([x1, y1, width, height])
+                    boxes.append(box)
+
+        indices = cv2.dnn.NMSBoxes(boxes, confidences, 0.5, 0.5)
+
+        for i in indices:
+            i = i[0]
+            x1, y1, w, h = boxes[i]
+            label = self.classes[classes_ids[i]]
+            conf = confidences[i]
+            # Added a space before {:.2f}
+            text = label + " {:.2f}".format(conf)
+            # Get the color for the class
+            class_color = self.class_colors[classes_ids[i]]
+
+            cv2.rectangle(img, (x1, y1), (x1 + w, y1 + h), class_color, 2)
+            cv2.putText(img, text, (x1, y1 - 2),
+                        cv2.FONT_HERSHEY_COMPLEX, 0.7, (255, 0, 255), 2)
+
+
 class FirstWindow(Screen):
-
     Builder.load_file('firstwindow.kv')
-    
+
+
 class WindowManager(ScreenManager):
     pass
 
 
 class rawApp(MDApp):
-
     def build(self):
-
         return WindowManager()
 
 
